edf43fadbedb62d53306d562a70b081135257598
juniperrobotics <271018@sycamoreschools.org>
2026-01-20 20:15:23 -0500

    Updated Distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Spindex/ColorSensorFSM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Spindex/ColorSensorFSM.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode.Spindex;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.core.HWMapSpindex;
 import org.firstinspires.ftc.teamcode.core.MotorWrapper;
-@TeleOp
 public class ColorSensorFSM{
     public enum states{
         EMPTY,
